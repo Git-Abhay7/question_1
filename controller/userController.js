a02f822562ae8c0481cd59077ca836d0a2a36c62
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-07-03 16:58:08 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -8,28 +8,27 @@ module.exports = {
         var query = { email: req.body.email }
         user.findOne(query, (error, result) => {
             if (error) {
-                res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
-            }
+                        res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
+                       }
             else if (result) {
                 if (result.email == req.body.email) {
                     res.send({ responseCode: 409, responseMessage: "Email already exist." })
-                }
-            }
+                                                     }
+                              }
             else {
                 const saltRounds = 10;
                 bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-                    req.body.password = hash
-                 
-                    new user(req.body).save((error, saved) => {
+                req.body.password = hash
+                new user(req.body).save((error, saved) => {
                         if (error) {
                             res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
-                        }
+                                   }
                         else {
                             res.send({ responseCode: 200, responseMessage: "Signup successfully", saved })
-                        }
+                             }
                     })
                 });
             }
         })
-    },
+    }
 }
