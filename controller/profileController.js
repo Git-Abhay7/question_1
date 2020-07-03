7d9fd48d65a630cda929c9fee1f213a706a2fcdc
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-07-03 16:34:37 +0530

    Update profileController.js

--- controller/profileController.js
+++ controller/profileController.js
@@ -4,27 +4,27 @@ module.exports = {
 
     profile: (req, res) => {
         if (!req.body.user_id) {
-            res.send({ responseCode: 404, responseMessage: "User id is required !" });
-        }
+                               res.send({ responseCode: 404, responseMessage: "User id is required !" });
+                               }
         else {
             profile.findOne({ user_id: req.body.user_id }, (error, result) => {
                 if (error) {
                     res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
-                }
-                if (result) {
+                           }
+               else if(result) {
                     res.send({ responseCode: 409, responseMessage: "User already exist." })
                 }
                 else {
-                    new profile(req.body).save((error, saved) => {
+                      new profile(req.body).save((error, saved) => {
                         if (error) {
-                            res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
-                        }
+                                    res.send({ responseCode: 500, responseMessage: "Internal server error.", error })
+                                   }
                         else {
-                            res.send({ responseCode: 200, responseMessage: "profile added successfully", saved })
-                        }
-                    })
-                }
-            })
+                              res.send({ responseCode: 200, responseMessage: "profile added successfully", saved })
+                             }
+                          })
+                     }
+               })
         }
     }
 }
